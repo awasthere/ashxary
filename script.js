6f5adf9a41193e231560aea5f3489b044b951d69
Aryan <158206428+awasthere@users.noreply.github.com>
2024-11-15 20:26:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+
+// Reveal Surprise Function
+function revealSurprise() {
+  document.getElementById("messageBox").classList.add("hidden");
+  document.getElementById("surpriseMessage").classList.remove("hidden");
+}
